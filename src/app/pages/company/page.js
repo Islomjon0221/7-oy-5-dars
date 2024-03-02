dab643f9f03f6eac84dbf18d886a491fb760ac15
Islomjon0221 <islomjon20110221@gmail.com>
2024-03-02 22:02:39 +0500

    Vazifa tayyor

--- src/app/pages/company/page.js
+++ src/app/pages/company/page.js
@@ -1,45 +1,20 @@
+import React from 'react'
 import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
+import styles from "../../page.module.css"
+import Link from 'next/link';
 
-export default function Home() {
+function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
+    <div className={styles.company}>
+      <h1>Company</h1>
+      <Image
+              src="/car.png"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              className={styles.companyImg}
+              width={600}
+              height={500}
               priority
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
         <Link
           href="/"
@@ -83,6 +58,8 @@ export default function Home() {
           </p>
         </Link>
       </div>
-    </main>
-  );
+    </div>
+  )
 }
+
+export default page
